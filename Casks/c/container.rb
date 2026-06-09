307996f1b0ca6d478ddf7852235f85d6f1fe36e0
OTSGE <199041525+otsge@users.noreply.github.com>
2026-06-09 04:12:05 +0000

    container (new cask)

---
+++ Casks/c/container.rb
@@ -0,0 +1,31 @@
+cask "container" do
+  version "1.0.0"
+  sha256 "13f45f26da94c354adcbefe1e8f7631e7f126e93c5d4dd6a5a538aa66b4f479d"
+
+  url "https://github.com/apple/container/releases/download/#{version}/container-#{version}-installer-signed.pkg"
+  name "container"
+  desc "Create and run Linux containers using lightweight virtual machines"
+  homepage "https://github.com/apple/container"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on arch: :arm64
+  depends_on macos: :sequoia
+
+  pkg "container-#{version}-installer-signed.pkg"
+
+  # container APIs aren't guaranteed to be backward compatible,
+  # so we stop the system service to ensure no components are out of sync.
+  # Ref: https://github.com/apple/container/issues/551#issuecomment-3246928923
+  postflight do
+    system_command "/usr/local/bin/container",
+                   args: ["system", "stop"]
+  end
+
+  uninstall pkgutil: "com.apple.container-installer"
+
+  zap trash: "~/Library/Application Support/com.apple.container"
+end
