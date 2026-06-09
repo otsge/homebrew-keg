7e554be7be9fa16b66f2c8747be74b161f54803b
John Faust <45057319+slickag@users.noreply.github.com>
2026-06-09 00:40:21 +0000

    stash-app@dev 0.31.1-80-ge28ab14c
    Closes #16.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/s/stash-app@dev.rb
+++ Casks/s/stash-app@dev.rb
@@ -1,5 +1,5 @@
 cask "stash-app@dev" do
-  version "0.31.1-79-gc39e1657"
+  version "0.31.1-80-ge28ab14c"
   sha256 :no_check
 
   url "https://github.com/stashapp/stash/releases/download/latest_develop/Stash.app.zip",
