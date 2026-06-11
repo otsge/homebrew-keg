3cb6ab6eb347ae8360338adcac3ed8de3d43b1b7
OTSGE <199041525+otsge@users.noreply.github.com>
2026-06-11 22:14:57 +0000

    radicle 0.12.0 (new cask)

---
+++ Casks/r/radicle.rb
@@ -0,0 +1,31 @@
+cask "radicle" do
+  version "0.12.0"
+  sha256 :no_check
+
+  url "https://files.radicle.dev/releases/radicle-desktop/latest/radicle-desktop-aarch64.dmg"
+  name "Radicle Desktop"
+  desc "Desktop client for Radicle repositories"
+  homepage "https://desktop.radicle.dev/"
+
+  livecheck do
+    url "https://files.radicle.dev/releases/radicle-desktop/latest/latest.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  depends_on :macos
+  depends_on arch: :arm64
+
+  app "Radicle.app"
+
+  postflight do
+    system_command "xattr",
+                   args: ["-c", "#{appdir}/Radicle.app"]
+  end
+
+  uninstall quit: "dev.radicle.desktop"
+
+  # zap trash: [
+  # ]
+end
