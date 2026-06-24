3fe09221a4ea9e8ba6bd9b267bba0456ecf0df72
OTSGE <199041525+otsge@users.noreply.github.com>
2026-06-24 18:56:39 +0000

    videoduplicatefinder@4 4.0-8d24b3d
    Closes #56.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/v/videoduplicatefinder@4.rb
+++ Casks/v/videoduplicatefinder@4.rb
@@ -1,13 +1,24 @@
 cask "videoduplicatefinder@4" do
   arch arm: "arm64", intel: "x64"
 
-  version :latest
+  version "4.0-8d24b3d"
   sha256 :no_check
 
   url "https://github.com/0x90d/videoduplicatefinder/releases/download/4.0.x/GUI-osx-#{arch}.tar.gz"
   name "Video Duplicate Finder"
   desc "Finds duplicated video (and image) files"
   homepage "https://github.com/0x90d/videoduplicatefinder"
+
+  livecheck do
+    url :url
+    regex(/Last\sbuild\sbased\son\s([0-9a-f]{7})/i)
+    strategy :github_latest do |json, regex|
+      match = json["body"]&.match(regex)
+      next if match.blank?
+
+      "4.0-#{match[1]}"
+    end
+  end
 
   depends_on macos: :big_sur
   depends_on formula: "ffmpeg@8"
