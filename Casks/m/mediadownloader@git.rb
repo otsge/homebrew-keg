24a64cd93352b22d56f5a90f026d2584bb9558cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 10:54:23 +0000

    mediadownloader@git 5.6.2.202607010909
    Closes #79.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/m/mediadownloader@git.rb
+++ Casks/m/mediadownloader@git.rb
@@ -1,5 +1,5 @@
 cask "mediadownloader@git" do
-  version "5.6.1.202606252353"
+  version "5.6.2.202607010909"
   sha256 :no_check
 
   url "https://github.com/mhogomchungu/media-downloader-git/releases/download/0.0.0/MediaDownloaderQt6.arm64.git.dmg"
