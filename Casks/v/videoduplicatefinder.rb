6eba15d9b88830fde9b2ea8ec5dd7e8d1a545405
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 14:03:10 +0000

    videoduplicatefinder 4.0-432-3fd7290
    Closes #99.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/v/videoduplicatefinder.rb
+++ Casks/v/videoduplicatefinder.rb
@@ -1,7 +1,7 @@
 cask "videoduplicatefinder" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.0-430-395d74a"
+  version "4.0-432-3fd7290"
   sha256 :no_check
 
   url "https://github.com/0x90d/videoduplicatefinder/releases/download/4.0.x/GUI-osx-#{arch}.tar.gz"
