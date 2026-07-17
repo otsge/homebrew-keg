cdaf5d676be69a48fc760bbbd6aa39b1110152ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-17 10:17:10 +0000

    fluxdown 0.2.3
    Closes #115.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/f/fluxdown.rb
+++ Casks/f/fluxdown.rb
@@ -1,9 +1,9 @@
 cask "fluxdown" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.2"
-  sha256 arm:   "2dc4b41b0f775c14115c002f60976931ae022866d387ef959eb48767068cd7a8",
-         intel: "f2d8595b21254a02e23ac2e6b5b8e2fa9fe9f3b976adc95a0c76b6c1c2f322a0"
+  version "0.2.3"
+  sha256 arm:   "581b837a5b9c890f9735c0f8ab8e94bc29dec974137258f125fd05cd2ca54189",
+         intel: "054db3019196bdadbabff24685c55f0c571086901b7be4209a3c9dc0e95492a8"
 
   url "https://github.com/zerx-lab/FluxDown/releases/download/v#{version}/FluxDown-#{version}-macos-#{arch}.dmg",
       verified: "github.com/zerx-lab/FluxDown/"
