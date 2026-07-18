ab0b87db916bfd38b94e27b75fc0cb6f866c83dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 21:14:50 +0000

    mediadownloader@git 5.6.3.202607182001

--- Casks/m/mediadownloader@git.rb
+++ Casks/m/mediadownloader@git.rb
@@ -1,5 +1,5 @@
 cask "mediadownloader@git" do
-  version "5.6.2.202607181827"
+  version "5.6.3.202607182001"
   sha256 :no_check
 
   url "https://github.com/mhogomchungu/media-downloader-git/releases/download/0.0.0/MediaDownloaderQt6.arm64.git.dmg"
