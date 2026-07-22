2057abe6d84e0e0ddf182564ce258ccf1eeb4cf1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-22 04:45:01 +0000

    stash-app@dev 0.31.1-142-gb6725dfd

--- Casks/s/stash-app@dev.rb
+++ Casks/s/stash-app@dev.rb
@@ -1,5 +1,5 @@
 cask "stash-app@dev" do
-  version "0.31.1-140-g634f567e"
+  version "0.31.1-142-gb6725dfd"
   sha256 :no_check
 
   url "https://github.com/stashapp/stash/releases/download/latest_develop/Stash.app.zip",
