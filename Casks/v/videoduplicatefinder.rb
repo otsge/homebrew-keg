d0cef461e90fa20c249964a2dedf444d5d3a1c4f
John Faust <45057319+slickag@users.noreply.github.com>
2026-07-23 12:50:16 +0000

    videoduplicatefinder 4.0-443-8fc5128
    Closes #134.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/v/videoduplicatefinder.rb
+++ Casks/v/videoduplicatefinder.rb
@@ -1,10 +1,10 @@
 cask "videoduplicatefinder" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.0-442-719ff75"
+  version "4.1-443-8fc5128"
   sha256 :no_check
 
-  url "https://github.com/0x90d/videoduplicatefinder/releases/download/4.0.x/GUI-osx-#{arch}.tar.gz"
+  url "https://github.com/0x90d/videoduplicatefinder/releases/download/4.1.x/GUI-osx-#{arch}.tar.gz"
   name "Video Duplicate Finder"
   desc "Finds duplicated video (and image) files"
   homepage "https://github.com/0x90d/videoduplicatefinder"
@@ -18,7 +18,7 @@ cask "videoduplicatefinder" do
         version = runs["head_sha"]&.match(regex)
         next if build.blank? || version.blank?
 
-        "4.0-#{build}-#{version}"
+        "4.1-#{build}-#{version}"
       end
     end
   end
