4206095b12789cefba24e01286fd6b5213d35ca2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-31 08:16:06 +0000

    stash-app@dev 0.31.1-152-g2926d02c
    Closes #147.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/s/stash-app@dev.rb
+++ Casks/s/stash-app@dev.rb
@@ -1,5 +1,5 @@
 cask "stash-app@dev" do
-  version "0.31.1-146-gafdaa082"
+  version "0.31.1-152-g2926d02c"
   sha256 :no_check
 
   url "https://github.com/stashapp/stash/releases/download/latest_develop/Stash.app.zip",
