17b850395e6640ef688c1ba8fb89c35334040f8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 08:35:00 +0000

    mediadownloader 5.6.5
    Closes #167.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/m/mediadownloader.rb
+++ Casks/m/mediadownloader.rb
@@ -1,6 +1,6 @@
 cask "mediadownloader" do
-  version "5.6.4"
-  sha256 "dd1517fd44367cec3a6e981a412f70831c1444f3abc102419543846395fd3fc2"
+  version "5.6.5"
+  sha256 "a5e4ba4be7024b236eebece79c9d19352cd393f2ad366b40130c83e9e43b6a3b"
 
   url "https://github.com/mhogomchungu/media-downloader/releases/download/#{version}/MediaDownloaderQt6-arm64-#{version}.dmg"
   name "Media Downloader"
