d8b6a0892ccccb4a84e4005de57254f2237936b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 06:53:24 +0000

    fluxdown 0.4.1

--- Casks/f/fluxdown.rb
+++ Casks/f/fluxdown.rb
@@ -1,9 +1,9 @@
 cask "fluxdown" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.3.2"
-  sha256 arm:   "e0dc91655b1507de5aeb57f46fedafa133f86d1854707c0a9b323d181d9cdad9",
-         intel: "0421d474da3a145205d02e4d0740e84de5c3594fce2903ddc33e7a73b7fd0172"
+  version "0.4.1"
+  sha256 arm:   "1ad0ec18583be3a195f804b366ac69672d9f654239f88e8fcd1d247743fe553a",
+         intel: "941377715fccc46a51a2e651017cf3e7894c17288bc3db811c627cc3235d00e9"
 
   url "https://github.com/zerx-lab/FluxDown/releases/download/v#{version}/FluxDown-#{version}-macos-#{arch}.dmg",
       verified: "github.com/zerx-lab/FluxDown/"
