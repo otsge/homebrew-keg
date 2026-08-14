1f8548a286d642df89363f5e655c1bdc0221bffe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 17:27:55 +0000

    radicle-desktop 0.15.0

--- Casks/r/radicle-desktop.rb
+++ Casks/r/radicle-desktop.rb
@@ -1,5 +1,5 @@
 cask "radicle-desktop" do
-  version "0.14.0"
+  version "0.15.0"
   sha256 :no_check
 
   url "https://files.radicle.dev/releases/radicle-desktop/latest/radicle-desktop-aarch64.dmg"
