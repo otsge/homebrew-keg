4ebf8f0ac3d9804758b50c04f8d3e74c93cf9e74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 03:15:05 +0000

    videoduplicatefinder 4.1-453-2a9c2e0

--- Casks/v/videoduplicatefinder.rb
+++ Casks/v/videoduplicatefinder.rb
@@ -1,7 +1,7 @@
 cask "videoduplicatefinder" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.1-452-35360a8"
+  version "4.1-453-2a9c2e0"
   sha256 :no_check
 
   url "https://github.com/0x90d/videoduplicatefinder/releases/download/4.1.x/GUI-osx-#{arch}.tar.gz"
