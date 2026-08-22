8cf4d5ea059982817c6470368180307b65b9404f
OTSGE <199041525+otsge@users.noreply.github.com>
2026-08-22 08:21:59 +0000

    rustdesk@nightly 1.4.9,1787380528000 (new cask)
    Closes #210.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

---
+++ Casks/r/rustdesk@nightly.rb
@@ -0,0 +1,59 @@
+cask "rustdesk@nightly" do
+  arch arm: "aarch64", intel: "x86_64"
+  url_end = on_system_conditional macos: ".dmg", linux: ".AppImage"
+
+  version "1.4.9,1787380528000"
+
+  on_macos do
+    sha256 arm:   "9bed6b180124eddb868f1098c392a1ebd60dd1468f7fc36c91d0bf1d0a83237d",
+           intel: "2f3ba30584d5ace7316eb37a5bf40902c792562834f202911e80b5ea17f8fed6"
+
+    depends_on macos: :monterey
+
+    app "RustDesk.app"
+
+    uninstall quit: "com.carriez.rustdesk"
+
+    zap trash: [
+      "/Library/LaunchAgents/com.carriez.RustDesk_server.plist",
+      "/Library/LaunchDaemons/com.carriez.RustDesk_service.plist",
+      "~/Library/Logs/RustDesk",
+      "~/Library/Preferences/com.carriez.RustDesk",
+      "~/Library/Saved Application State/com.carriez.rustdesk.savedState",
+    ]
+  end
+  on_linux do
+    sha256 arm64_linux:  "f161bacd1f11417000d5ca738b4114042e75f145911941e646f3c6357d4602ea",
+           x86_64_linux: "9358a87edd11a7cce90342549d3099cad50271b5314351d3b1e03851e597834e"
+
+    app_image "rustdesk-#{version.csv.first}-#{arch}.AppImage",
+              target: "RustDeskNightly.AppImage"
+  end
+
+  url "https://github.com/rustdesk/rustdesk/releases/download/nightly/rustdesk-#{version.csv.first}-#{arch}#{url_end}"
+  name "RustDesk"
+  desc "Open source virtual/remote desktop application"
+  homepage "https://rustdesk.com/"
+
+  livecheck do
+    url :url
+    regex(/^rustdesk[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}#{url_end}$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["tag_name"] != "nightly"
+        next if release["draft"]
+
+        release["assets"]&.map do |asset|
+          match = asset["name"]&.match(regex)
+          next if match.blank?
+
+          updated = asset["updated_at"]
+
+          "#{match[1]},#{DateTime.parse(updated).strftime("%Q")}"
+        end
+      end.flatten
+    end
+  end
+
+  conflicts_with cask: "rustdesk"
+end
