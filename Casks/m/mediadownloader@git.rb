a3a7c8ae9b0dd624ee60fd2ad0c17f0d5969266a
John Faust <45057319+slickag@users.noreply.github.com>
2026-09-09 11:51:39 +0000

    mediadownloader@git 5.6.6.202609091135

--- Casks/m/mediadownloader@git.rb
+++ Casks/m/mediadownloader@git.rb
@@ -1,5 +1,5 @@
 cask "mediadownloader@git" do
-  version "5.6.6.202609090731"
+  version "5.6.6.202609091135"
   sha256 :no_check
 
   url "https://github.com/mhogomchungu/media-downloader-git/releases/download/0.0.0/MediaDownloaderQt6.arm64.git.dmg"
